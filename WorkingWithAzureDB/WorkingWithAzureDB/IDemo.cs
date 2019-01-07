17e0b8c5827b32febe08f5f050cde9aefa3f42c5
AlexandraChiritaACS <46438366+AlexandraChiritaACS@users.noreply.github.com>
2019-01-07 09:36:35 +0200

    Update IDemo.cs

--- WorkingWithAzureDB/WorkingWithAzureDB/IDemo.cs
+++ WorkingWithAzureDB/WorkingWithAzureDB/IDemo.cs
@@ -1,34 +1,37 @@
 ﻿namespace WorkingWithAzureDB
 {
+     // Abstract class for testing the DB
     public abstract class IDemo
     {
+        // Display customer list
         public abstract void ShowAllCustomers(int customerID = 0);
-
+        // Methods for inserting row in DB (to be implemented in the extending class)
         public abstract void InsertNewCustomer(Customer givenCustomer);
-
+        // Methods for update row (change atribute) in DB (to be implemented in the extending class)
         public abstract void UpdateCustomerName(int customerID, string firstName, string lastName);
-
+        // Methods for deleting a row in DB (to be implemented in the extending class)
         public abstract void DeleteCustomer(int customerID);
-
+        
+        // Testing the functionality of the DB
         public virtual void RunDemo(Customer newCustomer)
         {
-            // What am I doing Here
+            // By every step, the Customers list is displayed
             ShowAllCustomers();
             System.Console.WriteLine("SELECTED EVERYTHING..." + System.Environment.NewLine);
 
-            // What am I doing Here
+            
             System.Console.WriteLine("Adding myself" + System.Environment.NewLine);
             ShowAllCustomers(newCustomer.CustomerID);
             InsertNewCustomer(newCustomer);
             System.Console.WriteLine("Added Myself..." + System.Environment.NewLine);
 
-            // What am I doing Here
+            
             ShowAllCustomers(newCustomer.CustomerID);
             System.Console.WriteLine("Updating myself..." + System.Environment.NewLine);
             UpdateCustomerName(newCustomer.CustomerID, newCustomer.LastName, newCustomer.FirstName);
             ShowAllCustomers(newCustomer.CustomerID);
 
-            // What am I doing Here
+            
             System.Console.WriteLine("Erasing myself..." + System.Environment.NewLine);
             DeleteCustomer(newCustomer.CustomerID);
             ShowAllCustomers(newCustomer.CustomerID);
