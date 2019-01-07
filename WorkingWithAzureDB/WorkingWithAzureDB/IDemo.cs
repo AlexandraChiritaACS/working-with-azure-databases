9cee00b3b842b8a6697fdce1695d758847ddcb9e
AlexandraChiritaACS <46438366+AlexandraChiritaACS@users.noreply.github.com>
2019-01-07 10:09:51 +0200

    Update IDemo.cs

--- WorkingWithAzureDB/WorkingWithAzureDB/IDemo.cs
+++ WorkingWithAzureDB/WorkingWithAzureDB/IDemo.cs
@@ -2,33 +2,34 @@
 {
     public abstract class IDemo
     {
+        // DIsplay the customers list
         public abstract void ShowAllCustomers(int customerID = 0);
-
+        // Inserting a row in DB
         public abstract void InsertNewCustomer(Customer givenCustomer);
-
+        // Update a row in DB
         public abstract void UpdateCustomerName(int customerID, string firstName, string lastName);
-
+        // Delete a row from DB
         public abstract void DeleteCustomer(int customerID);
 
         public virtual void RunDemo(Customer newCustomer)
         {
-            // What am I doing Here
+            // Display initial customer list
             ShowAllCustomers();
             System.Console.WriteLine("SELECTED EVERYTHING..." + System.Environment.NewLine);
 
-            // What am I doing Here
+            // Test inserting
             System.Console.WriteLine("Adding myself" + System.Environment.NewLine);
             ShowAllCustomers(newCustomer.CustomerID);
             InsertNewCustomer(newCustomer);
             System.Console.WriteLine("Added Myself..." + System.Environment.NewLine);
 
-            // What am I doing Here
+            // Test updating
             ShowAllCustomers(newCustomer.CustomerID);
             System.Console.WriteLine("Updating myself..." + System.Environment.NewLine);
             UpdateCustomerName(newCustomer.CustomerID, newCustomer.LastName, newCustomer.FirstName);
             ShowAllCustomers(newCustomer.CustomerID);
 
-            // What am I doing Here
+            // Test deleting
             System.Console.WriteLine("Erasing myself..." + System.Environment.NewLine);
             DeleteCustomer(newCustomer.CustomerID);
             ShowAllCustomers(newCustomer.CustomerID);
